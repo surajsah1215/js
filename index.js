852fffc7f9501ba4280d0c5847d92035fd3054dc
suraj <surajsah1215@gmail.com>
2023-05-18 16:01:57 +0530

    getbyclassName

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ bord.style.borderBottom = 'solid 4px black';
 const item = document.getElementsByClassName('title')[0];
 item.style.fontWeight = '900';
 item.style.color = 'green';
+
+const li = document.getElementsByClassName('list-group-item');
+li[2].style.backgroundColor = 'green';
+
+for(let i=0; i<li.length; i++){
+    li[i].style.fontWeight = '900';
+}
