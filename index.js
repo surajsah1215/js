8f520821674fa84c64047043dbe1ad1a800c1fa7
suraj <surajsah1215@gmail.com>
2023-05-19 18:53:01 +0530

    filter

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 var form = document.getElementById('addForm');
 var items = document.getElementById('items');
 var filter = document.getElementById('filter');
+var input = document.getElementById('item');
+var edittb = document.querySelectorAll('li');
 
 filter.addEventListener('keyup', filterItems);
 items.addEventListener('click',removeItem);
-
 form.addEventListener('submit',addItem);
+
+
+const desc = document.createElement('input');
+desc.className = 'form-control mr-2';
+form.appendChild(desc);
+
+const li = document.getElementsByClassName('list-group-item');
+
+for(let i=0; i<edittb.length; i++){
+    const editBtn = document.createElement('button');
+
+    li[i].appendChild(editBtn);
+    editBtn.className = 'float-right';
+    editBtn.classList.add('edit');
+    editBtn.innerText = 'Edit';
+    items.appendChild(li[3]);
+
+   
+}
 
 function addItem(e){
     e.preventDefault();
     const itemValue = document.getElementById('item').value;
+    const itemValue2 = desc.value;
 
     var li = document.createElement('li');
     var button = document.createElement('button');
@@ -17,6 +38,7 @@ function addItem(e){
 
     li.className = 'list-group-item';
     li.appendChild(document.createTextNode(itemValue));
+    li.appendChild(document.createTextNode(itemValue2));
 
     
     li.appendChild(button);
@@ -50,7 +72,7 @@ function filterItems(e){
     var li = items.getElementsByTagName('li');
 
     Array.from(li).forEach(function(item){
-        var itemName = item.firstChild.textContent;
+        var itemName = item.textContent;
         if(itemName.toLowerCase().indexOf(text) != -1){
             item.style.display = 'block';
           } else {
