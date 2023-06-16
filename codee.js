f0b4fdce95a8d9be8efdbd71f3c41cdbfc193463
suraj <surajsah1215@gmail.com>
2023-06-16 22:54:31 +0530

    hello world

--- codee.js
+++ codee.js
@@ -1,13 +1,1 @@
-let str = "abbc";
-let arr =[]
-for(let i=0; i<str.length; i++){
-    for(let j=0; j<arr.length; j++){
-    if(arr[j] == str[i]){
-        arr.pop();
-        
-    }
-}
-    arr[i] = str[i]
-}
-console.log(arr.toString());
-console.log(str)
+console.log('hello world');
