dd9e0ac5a2de5a49ab173bb97a4e55fe770a88ab
suraj <surajsah1215@gmail.com>
2023-06-17 12:58:05 +0530

    object

--- codee.js
+++ codee.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const mul = (a,b)=>{
+    return a*b;
+}
+
+console.log(mul(3,4));
+
+const student={
+name:'max',
+age:23,
+ print(){
+    console.log('my name is ' + this.name + ' and age is '+ this.age )
+}
+}
+
+student.print();
