27cccc150ae6fee91393e4fea8be3830b01dfdc7
suraj <surajsah1215@gmail.com>
2023-06-23 14:07:31 +0530

    dynamic routes

--- 00-starting-setup/routes/shop.js
+++ 00-starting-setup/routes/shop.js
@@ -2,10 +2,20 @@ const path = require('path');
 
 const express = require('express');
 
-const getproductcontroller = require('../controllers/product')
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/',getproductcontroller.getproducts );
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId',shopController.getProduct)
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
